7b48d5a6e0c97739f9227baa1737d3a213c8d6f3
Mustufa786 <mustufa_ayub@hotmail.com>
2020-01-13 19:13:46 +0500

    some changes

--- app/src/main/java/edu/aku/hassannaqvi/tpvics_hh/ui/SectionCActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/tpvics_hh/ui/SectionCActivity.java
@@ -1,7 +1,11 @@
 package edu.aku.hassannaqvi.tpvics_hh.ui;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
@@ -123,9 +127,6 @@ public class SectionCActivity extends AppCompatActivity {
     private void SaveDraft() {
     }
 
-    private boolean formValidation() {
-        return Validator.emptyCheckingContainer(this, bi.GrpName);
-    }
 
     public void BtnEnd() {
 
