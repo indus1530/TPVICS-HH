34d5f7e624813d45ad7b23ca222c8a9417b5c2f0
Mustufa786 <mustufa_ayub@hotmail.com>
2020-01-13 15:43:48 +0500

    section B layout done

---
+++ app/src/main/java/edu/aku/hassannaqvi/tpvics_hh/ui/SectionBActivity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.tpvics_hh.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.aku.hassannaqvi.tpvics_hh.R;
+
+public class SectionBActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_b);
+    }
+}
