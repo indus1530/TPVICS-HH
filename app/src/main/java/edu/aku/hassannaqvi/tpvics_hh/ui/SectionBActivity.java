a788dddf8faa31f5cb746da07c0bad0c29b95f50
muhammad.farooqui <waqarfarooqui.12@gmail.com>
2020-01-13 16:44:34 +0500

    fixed save draft in section B activity.

--- app/src/main/java/edu/aku/hassannaqvi/tpvics_hh/ui/SectionBActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/tpvics_hh/ui/SectionBActivity.java
@@ -53,16 +53,7 @@ public class SectionBActivity extends AppCompatActivity {
         }
     }
 
-    private boolean UpdateDB() {
-
-        return true;
-    }
-
-    private boolean formValidation() {
-        return Validator.emptyCheckingContainer(this, bi.fldGrpSecB);
-    }
-
-    private void saveDraft() throws JSONException {
+    private void SaveDraft() throws JSONException {
         JSONObject f1 = new JSONObject();
 
         //f1.put("hl01", bi.hl01.getText().toString());
@@ -71,25 +62,25 @@ public class SectionBActivity extends AppCompatActivity {
 
         f1.put("hl03", bi.hl03a.isChecked() ? "1" :
                 bi.hl03b.isChecked() ? "2" :
-                bi.hl03c.isChecked() ? "3" :
-                bi.hl03d.isChecked() ? "4" :
-                bi.hl03e.isChecked() ? "5" :
-                bi.hl03f.isChecked() ? "6" :
-                bi.hl03g.isChecked() ? "7" :
-                bi.hl03h.isChecked() ? "8" :
-                bi.hl03i.isChecked() ? "9" :
-                bi.hl03j.isChecked() ? "10" :
-                bi.hl03k.isChecked() ? "11" :
-                bi.hl03l.isChecked() ? "12" :
-                bi.hl03m.isChecked() ? "13" :
-                bi.hl03n.isChecked() ? "14" :
-                bi.hl03o.isChecked() ? "15" :
-                bi.hl03p.isChecked() ? "96" :
-                bi.hl03q.isChecked() ? "98" : "0");
+                        bi.hl03c.isChecked() ? "3" :
+                                bi.hl03d.isChecked() ? "4" :
+                                        bi.hl03e.isChecked() ? "5" :
+                                                bi.hl03f.isChecked() ? "6" :
+                                                        bi.hl03g.isChecked() ? "7" :
+                                                                bi.hl03h.isChecked() ? "8" :
+                                                                        bi.hl03i.isChecked() ? "9" :
+                                                                                bi.hl03j.isChecked() ? "10" :
+                                                                                        bi.hl03k.isChecked() ? "11" :
+                                                                                                bi.hl03l.isChecked() ? "12" :
+                                                                                                        bi.hl03m.isChecked() ? "13" :
+                                                                                                                bi.hl03n.isChecked() ? "14" :
+                                                                                                                        bi.hl03o.isChecked() ? "15" :
+                                                                                                                                bi.hl03p.isChecked() ? "96" :
+                                                                                                                                        bi.hl03q.isChecked() ? "98" : "0");
 
         f1.put("hl04", bi.hl04a.isChecked() ? "1" :
                 bi.hl04b.isChecked() ? "2" :
-                bi.hl04c.isChecked() ? "3" : "0");
+                        bi.hl04c.isChecked() ? "3" : "0");
 
         f1.put("hl05", bi.hl05.getText().toString());
         //f1.put("hl05dd", bi.hl05dd.getText().toString());
@@ -106,32 +97,32 @@ public class SectionBActivity extends AppCompatActivity {
 
         f1.put("hl07", bi.hl07a.isChecked() ? "1" :
                 bi.hl07b.isChecked() ? "2" :
-                bi.hl07c.isChecked() ? "3" :
-                bi.hl07d.isChecked() ? "4" :
-                bi.hl07e.isChecked() ? "5" : "0");
+                        bi.hl07c.isChecked() ? "3" :
+                                bi.hl07d.isChecked() ? "4" :
+                                        bi.hl07e.isChecked() ? "5" : "0");
 
         f1.put("hl08", bi.hl08a.isChecked() ? "1" :
                 bi.hl08b.isChecked() ? "2" :
-                bi.hl08c.isChecked() ? "3" :
-                bi.hl08d.isChecked() ? "4" :
-                bi.hl08e.isChecked() ? "5" :
-                bi.hl08f.isChecked() ? "6" :
-                bi.hl08g.isChecked() ? "7" :
-                bi.hl08h.isChecked() ? "8" :
-                bi.hl08i.isChecked() ? "9" :
-                bi.hl08j.isChecked() ? "98" : "0");
+                        bi.hl08c.isChecked() ? "3" :
+                                bi.hl08d.isChecked() ? "4" :
+                                        bi.hl08e.isChecked() ? "5" :
+                                                bi.hl08f.isChecked() ? "6" :
+                                                        bi.hl08g.isChecked() ? "7" :
+                                                                bi.hl08h.isChecked() ? "8" :
+                                                                        bi.hl08i.isChecked() ? "9" :
+                                                                                bi.hl08j.isChecked() ? "98" : "0");
 
         f1.put("hl09", bi.hl09a.isChecked() ? "1" :
                 bi.hl09b.isChecked() ? "2" :
-                bi.hl09c.isChecked() ? "3" :
-                bi.hl09d.isChecked() ? "4" :
-                bi.hl09e.isChecked() ? "5" :
-                bi.hl09f.isChecked() ? "6" :
-                bi.hl09g.isChecked() ? "7" :
-                bi.hl09h.isChecked() ? "8" :
-                bi.hl09i.isChecked() ? "9" :
-                bi.hl09j.isChecked() ? "10" :
-                bi.hl0996.isChecked() ? "96" : "0");
+                        bi.hl09c.isChecked() ? "3" :
+                                bi.hl09d.isChecked() ? "4" :
+                                        bi.hl09e.isChecked() ? "5" :
+                                                bi.hl09f.isChecked() ? "6" :
+                                                        bi.hl09g.isChecked() ? "7" :
+                                                                bi.hl09h.isChecked() ? "8" :
+                                                                        bi.hl09i.isChecked() ? "9" :
+                                                                                bi.hl09j.isChecked() ? "10" :
+                                                                                        bi.hl0996.isChecked() ? "96" : "0");
         f1.put("hl0996x", bi.hl0996x.getText().toString());
 
         f1.put("hl10", bi.hl10a.isChecked() ? "1" :
@@ -158,6 +149,14 @@ public class SectionBActivity extends AppCompatActivity {
 
         f1.put("hl16a", bi.hl16a.getText().toString());
         f1.put("hl16b", bi.hl16b.getText().toString());
+    }
 
+    private boolean UpdateDB() {
+
+        return true;
+    }
+
+    private boolean formValidation() {
+        return Validator.emptyCheckingContainer(this, bi.fldGrpSecB);
     }
 }
