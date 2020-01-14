b442953e8253fef2070225892c09398825636f0a
muhammad.farooqui <waqarfarooqui.12@gmail.com>
2020-01-14 10:18:52 +0500

    divide 2 layout of section C activity.

--- app/src/main/java/edu/aku/hassannaqvi/tpvics_hh/ui/SectionC02Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/tpvics_hh/ui/SectionC02Activity.java
@@ -1,80 +1,59 @@
 package edu.aku.hassannaqvi.tpvics_hh.ui;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
-import com.validatorcrawler.aliazaz.Validator;
-
 import edu.aku.hassannaqvi.tpvics_hh.R;
 import edu.aku.hassannaqvi.tpvics_hh.core.MainApp;
-import edu.aku.hassannaqvi.tpvics_hh.databinding.ActivitySectionCBinding;
+import edu.aku.hassannaqvi.tpvics_hh.databinding.ActivitySectionC02Binding;
 import edu.aku.hassannaqvi.tpvics_hh.validation.ClearClass;
 import edu.aku.hassannaqvi.tpvics_hh.validation.ValidatorClass;
 
-public class SectionCActivity extends AppCompatActivity {
+public class SectionC02Activity extends AppCompatActivity {
 
-    private ActivitySectionCBinding bi;
+    private ActivitySectionC02Binding bi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_c);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_c02);
         bi.setCallback(this);
 
         setListeners();
     }
 
+
     private void setListeners() {
 
-        bi.hs04.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        bi.hs22.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
 
-                if (bi.hs04b.isChecked()) {
-                    ClearClass.ClearAllFields(bi.fldGrpCVhs05, null);
+                if (bi.hs22b.isChecked()) {
+                    ClearClass.ClearAllFields(bi.fldGrpCVhs23, null);
                 }
             }
         });
 
-        bi.hs07.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        bi.hs24.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
 
-                if (!bi.hs07h.isChecked() || !bi.hs07h.isChecked()) {
-                    ClearClass.ClearAllFields(bi.fldGrpCVhs08, null);
-                }
-            }
-        });
-
-        bi.hs09.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-
-                if (bi.hs09b.isChecked()) {
-                    ClearClass.ClearAllFields(bi.fldGrpCVhs0x, null);
-                }
-            }
-        });
-
-        bi.hs11.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-
-                if (bi.hs11b.isChecked()) {
-                    ClearClass.ClearAllFields(bi.fldGrpCVhs12, null);
+                if (bi.hs24b.isChecked()) {
+                    ClearClass.ClearAllFields(bi.fldGrpCVhs25, null);
                 }
             }
         });
     }
 
     private boolean formValidation() {
-        return ValidatorClass.EmptyCheckingContainer(this, bi.fldGrpSecC);
+        return ValidatorClass.EmptyCheckingContainer(this, bi.fldGrpSecC02);
     }
 
     public void BtnContinue() {
@@ -121,11 +100,11 @@ public class SectionCActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialogInterface, int i) {
                         SaveDraft();
                         if (!UpdateDB()) {
-                            Toast.makeText(SectionCActivity.this, "Error in updating db!!", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(SectionC02Activity.this, "Error in updating db!!", Toast.LENGTH_SHORT).show();
                             return;
                         }
 
-                        MainApp.endActivitySetRouting(SectionCActivity.this, SectionCActivity.this, EndingActivity.class, false, null);
+                        MainApp.endActivitySetRouting(SectionC02Activity.this, SectionC02Activity.this, EndingActivity.class, false, null);
                     }
                 })
                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
