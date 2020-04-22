138880740c72640559fb05bc8bd445feae86b6aa
SAJID LATIF <indus1530@gmail.com>
2020-04-22 15:53:04 +0500

    chc2 created

---
+++ app/src/main/java/edu/aku/hassannaqvi/tpvics_hh/ui/sections/SectionCHC2Activity.java
@@ -0,0 +1,15 @@
+package edu.aku.hassannaqvi.tpvics_hh.ui.sections;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import edu.aku.hassannaqvi.tpvics_hh.R;
+
+public class SectionCHC2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_chc2);
+    }
+}
